2ef3cdce94865da6f514b9e1afbb70ed45f6265a
Jose Cabral <jose12marte@yahoo.com>
2020-11-25 10:41:53 -0500

    Added class user for test

---
+++ CIS3270Project2/src/users/User.java
@@ -0,0 +1,29 @@
+package users;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private int zip;
+	private String state; //two letter code
+	private String userName;
+	private String password;
+	private String ssn; // social security number
+	private String securityQuestion;
+	
+	public User(String firstName, String lastName, String email, String address, int zip, String state, 
+			String userName, String password, String ssn, String securityQuestion) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.zip = zip;
+		this.state = state;
+		this.userName = userName;
+		this.password = password;
+		this.ssn = ssn;
+		this.securityQuestion = securityQuestion;
+	}
+}
